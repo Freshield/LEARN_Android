13a812f00b95ce6964c81d33009d8cdca908029b
freshield <zxdsw199182@gmail.com>
2017-05-27 16:51:32 -0400

    done chp1

--- Chp1/HelloWorld/app/src/main/java/com/example/freshield/helloworld/HelloWorldActivity.java
+++ Chp1/HelloWorld/app/src/main/java/com/example/freshield/helloworld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.example.freshield.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class HelloWorldActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world_layout);
+        Log.d("data", "onCreate execute");
+
     }
+
+    private static final String TAG = "HelloWorldActivity";
 }
