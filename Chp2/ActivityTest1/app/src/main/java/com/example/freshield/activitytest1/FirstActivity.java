1fa8580d6af8fcb4a159a451ac3b4551337c198b
freshield <zxdsw199182@gmail.com>
2017-05-28 19:57:43 -0400

    done second activity

--- Chp2/ActivityTest1/app/src/main/java/com/example/freshield/activitytest1/FirstActivity.java
+++ Chp2/ActivityTest1/app/src/main/java/com/example/freshield/activitytest1/FirstActivity.java
@@ -1,7 +1,14 @@
 package com.example.freshield.activitytest1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +16,39 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Toast.makeText(FirstActivity.this, "You clicked Button 1",Toast.LENGTH_SHORT).show();
+                //finish();
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        Log.d("create", "onCreate: "+this.toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                Log.d("try this name", this.toString());
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                Log.d("try this name", this.toString());
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
