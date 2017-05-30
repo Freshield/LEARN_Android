ae7dbe4c5fe8f6cfc0804961fcbe046677183f77
freshield <zxdsw199182@gmail.com>
2017-05-30 15:09:15 -0400

    done edit text

--- Chp3/HelloWorld/app/src/main/java/com/example/freshield/helloworld/MainActivity.java
+++ Chp3/HelloWorld/app/src/main/java/com/example/freshield/helloworld/MainActivity.java
@@ -4,15 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button)findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.edit_text);
         button.setOnClickListener(this);
         /*
         button.setOnClickListener(new View.OnClickListener(){
@@ -28,7 +32,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
-                Toast.makeText(MainActivity.this, "You click Button", Toast.LENGTH_SHORT).show();
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
         }
     }
 }
