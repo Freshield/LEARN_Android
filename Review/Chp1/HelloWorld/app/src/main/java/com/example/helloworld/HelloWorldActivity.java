914d369a719433262c82e53b959d838fc06cde22
freshield <zxdsw199182@gmail.com>
2017-07-23 15:22:51 -0400

    begin today

--- Review/Chp1/HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
+++ Review/Chp1/HelloWorld/app/src/main/java/com/example/helloworld/HelloWorldActivity.java
@@ -2,6 +2,7 @@ package com.example.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world_layout);
+        //Log.d("")
     }
 }
